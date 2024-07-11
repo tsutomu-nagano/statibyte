90089fc0b087856ef720cc2415641b992c38981c
tsutomu-nagano <copyxshow@gmail.com>
2024-07-11 12:47:37 +0900

    font,iconの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
+import icon from "astro-icon";
 
 export default defineConfig({
   site: 'https://tsutomu-nagano.github.io',
   base: '/statibyte',
+  integrations: [icon()],
 })
