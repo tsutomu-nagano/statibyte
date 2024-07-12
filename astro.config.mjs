73182346788d52881bab87df732dd140d2b0f7e2
tsutomu-nagano <copyxshow@gmail.com>
2024-07-12 19:27:50 +0900

    SSR用の処理追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
+import node from "@astrojs/node";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://tsutomu-nagano.github.io',
   base: '/statibyte',
   integrations: [icon()],
-})
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
